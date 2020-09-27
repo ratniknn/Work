b4ef653863cccb6cdb834572b8c99c2d8b7912ce
anton <antonaumov@gmail.com>
2020-09-27 16:22:51 +0300

    fist

--- script.js
+++ script.js
@@ -1,2 +1,20 @@
-console.log('Привет');
-let b;
+const numberOfFilms = +prompt('Сколько фильмов вы просмотрели?');
+
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+const a = prompt('Один из последних просмотренных фильмов?', ''),
+      b = prompt('На сколько оцените его?', ''),
+      c = prompt('Один из последних просмотренных фильмов?', ''),
+      d = prompt('На сколько оцените его?', '');
+      
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
+console.log(personalMovieDB);
+console.log('OK');
